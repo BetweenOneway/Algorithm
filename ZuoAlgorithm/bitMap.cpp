48e155c7113d3ba6744450765781042cc7de7f37
王者伟业 <wang1902568721@163.com>
2022-10-05 12:51:11 +0800

    add bit map

---
+++ ZuoAlgorithm/bitMap.cpp
@@ -0,0 +1,30 @@
+﻿#include "bitMap.h"
+
+BitMap::BitMap()
+{
+	arr.resize(10, 0);
+}
+
+//获取第n位的状态
+int BitMap::getBitStatus(int n)
+{
+	int numIndex = n / 32;
+	int bitIndex = n % 32;
+	return arr[numIndex] >> bitIndex & 1;
+}
+
+//将第n位设置为1或0
+bool BitMap::setBitStatus(int n,bool flag)
+{
+	int numIndex = n / 32;
+	int bitIndex = n % 32;
+	if (flag)
+	{
+		arr[numIndex] = arr[numIndex] & (1 << bitIndex);
+	}
+	else
+	{
+		arr[numIndex] = arr[numIndex] & (~(1 << bitIndex));
+	}
+	return true;
+}
---
+++ ZuoAlgorithm/bitMap.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <vector>
+using namespace std;
+
+class BitMap
+{
+private:
+	vector<int> arr;
+public:
+	BitMap();
+	int getBitStatus(int n);
+	bool setBitStatus(int n, bool flag);
+};
